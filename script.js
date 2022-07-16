39abe287e48c956c213248487d0e47fa01ff6a95
Gabriel Pierre <94745573+gabrielppierre@users.noreply.github.com>
2022-07-16 10:51:02 -0300

    aviso

---
+++ script.js
@@ -0,0 +1,8 @@
+var largura = window.screen.availWidth
+
+var altura = window.screen.height;
+var popup = document.getElementById("popup");
+
+if((largura >= 1250) && (altura >= 760)){
+    popup.style.display="none";
+};
